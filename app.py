7678ed9381489aca4e6e9ba7834a020884905060
kennymcmillan <kennymcmillan29@gmail.com>
2025-03-09 16:14:02 +0300

    init

---
+++ app.py
@@ -0,0 +1,19 @@
+import streamlit as st
+
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+
+def get_driver():
+    return webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+
+options = Options()
+options.add_argument('--disable-gpu')
+options.add_argument('--headless')
+
+driver = get_driver()
+driver.get('http://example.com')
+
+st.code(driver.page_source)
